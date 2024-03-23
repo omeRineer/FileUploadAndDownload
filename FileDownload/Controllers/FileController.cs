4914cda1e708d1604c3aba99f4052b32946e7dab
Ömer Faruk Sandıkçı <sandikci_omer@hotmail.com>
2024-03-23 13:20:35 +0300

    Add project files.

---
+++ FileDownload/Controllers/FileController.cs
@@ -0,0 +1,43 @@
+﻿using FileDownload.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileDownload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        readonly IFileService _fileService;
+
+        public FileController(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        [HttpPost("upload-file")]
+        public async Task<IActionResult> UploadFiles(IFormFileCollection files)
+        {
+            await _fileService.UploadFile(files);
+
+            return Ok();
+        }
+
+        [HttpGet("file-bytes")]
+        public async Task<IActionResult> ReturnFileByteArray(string fileName)
+        {
+            var (fileBytes, contentType) = await _fileService.ReturnFileByteArray(fileName);
+
+            return File(fileBytes, contentType, fileDownloadName: fileName);
+        }
+
+        [HttpGet("file-stream")]
+        public async Task<IActionResult> ReturnFileStream(string fileName)
+        {
+            var (fileStream, contentType) = await _fileService.ReturnFileStream(fileName);
+
+            return File(fileStream, contentType, fileDownloadName: fileName);
+
+        }
+    }
+}
---
+++ FileDownload/Services/FileService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileDownload.Services
+{
+    public interface IFileService
+    {
+        Task UploadFile(IFormFileCollection fileCollection);
+        Task<(byte[], string)> ReturnFileByteArray(string fileName);
+        Task<(FileStream, string)> ReturnFileStream(string fileName);
+    }
+
+    public class FileService : IFileService
+    {
+        readonly IHostEnvironment env;
+        readonly IConfiguration Configuration;
+        readonly IDictionary<string, string> ContentTypes;
+
+        public FileService(IHostEnvironment env, IConfiguration configuration)
+        {
+            this.env = env;
+            Configuration = configuration;
+            ContentTypes = Configuration.GetSection("ContentTypes")
+                                        .GetChildren()
+                                        .Select(s => new KeyValuePair<string, string>(s["Extension"], s["ContentType"]))
+                                        .ToDictionary(s => s.Key, s => s.Value);
+        }
+
+        public async Task<(byte[], string)> ReturnFileByteArray(string fileName)
+        {
+            var filePath = $"{env.ContentRootPath}/wwwroot/{fileName}";
+
+            var fileBytes = await File.ReadAllBytesAsync(filePath);
+
+            var contentType = ContentTypes.FirstOrDefault(f => f.Key == Path.GetExtension(fileName)).Value;
+
+            return (fileBytes, contentType);
+        }
+
+        public Task<(FileStream, string)> ReturnFileStream(string fileName)
+        {
+            var filePath = $"{env.ContentRootPath}/wwwroot/{fileName}";
+
+            var fileStream = File.OpenRead(filePath);
+
+            var contentType = ContentTypes.FirstOrDefault(f => f.Key == Path.GetExtension(fileName)).Value;
+
+            return Task.FromResult((fileStream, contentType));
+        }
+
+        public async Task UploadFile(IFormFileCollection fileCollection)
+        {
+            await Parallel.ForEachAsync(fileCollection, (file, ct) =>
+            {
+                var filePath = $"{env.ContentRootPath}/wwwroot/{Guid.NewGuid}{Path.GetExtension(file.FileName)}";
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyToAsync(fileStream);
+                }
+
+                return ValueTask.CompletedTask;
+            });
+        }
+    }
+}
